db7c90eae321ff043f29734d52746881512bdee6
Joshua Book <joshua.book@gmail.com>
2019-11-27 12:53:48 -0500

    WIP - move towards ROOMS.

--- index.js
+++ index.js
@@ -6,23 +6,65 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const subscribers = {};
-const rooms = [
-  {player1: 'text', player2: 'text'}
-]
+const players = [];
+
+const ROOMS = {};
+class Player {
+  constructor(name, socket, isRoomOwner) {
+    this.name = name;
+    this.socket = socket;
+    this.isRoomOwner = isRoomOwner;
+  }
+}
+
+class Room {
+  constructor(owner, visitor) {
+    this.owner = owner;
+    this.visitor = visitor;
+    this.name = owner.name;
+  }
+}
 
 io.on('connection', socket => {
-  const username = socket.handshake.query.username;
-  subscribers[username] = socket;
-  console.log('subscribers: ', subscribers);
+  const { username, roomname } = socket.handshake.query;
 
-  io.on(username, )
+  if (roomname === username) {
+    ROOMS[roomname] = new Room(new Player(username, socket, true));
+  } else {
+    ROOMS[roomname].visitor = new Player(username, socket, false);
+  }
+
+  const room = ROOMS[roomname];
+
+  socket.join(room.name);
+  socket.on(room.name, (from, msg) => {
+    console.log(`I received a private message by ${from} saying ${msg}`);
+  });
 });
 
-app.get('/', (req, res) => {
-  const { username } = req.query;
-  const subsList = Object.keys(subscribers).filter(sub => sub !== username);
-  res.send(subsList);
+// app.post('/signin', (req, res) => {
+//   const { username } = req.body;
+//   players.push(username);
+//   res.send(username);
+// });
+
+app.post('/exit', (req, res) => {
+  const { roomname, isRoomOwner } = req.body;
+  const room = rooms[roomname];
+
+  // fix up this logic so visitors are not stranded
+  if (isRoomOwner) {
+    delete rooms[roomname];
+  } else {
+    room.visitor = null;
+  }
+
+  res.send(rooms);
+});
+
+app.get('/rooms', (req, res) => {
+  console.log('ROOMS', ROOMS)
+  res.send(Object.values(ROOMS));
 });
 
 server.listen(9000, () => {
